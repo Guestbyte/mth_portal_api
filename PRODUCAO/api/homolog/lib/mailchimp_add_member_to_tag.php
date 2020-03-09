9b4107c6dec9d38ef272774772b79ed4e3de1960
Fernando Ortiz Mello <guestbyte@gmail.com>
2020-03-09 12:37:10 -0300

    fix error output on log

--- PRODUCAO/api/homolog/lib/mailchimp_add_member_to_tag.php
+++ PRODUCAO/api/homolog/lib/mailchimp_add_member_to_tag.php
@@ -20,12 +20,11 @@ function mailchimp_add_member_to_tag(array $mc_list_tags_names_ids, string $mc_l
 
         $mc_result_add_member_to_tag = $MailChimp->post("lists/$mc_list_id/segments/" . $tag_id, $data);
 
-        $tag_exist = (strpos($mc_result_add_member_to_tag['errors'][0]['error'], "already exist"));
-        $tag_added = ($mc_result_add_member_to_tag['members_added'][0]['status'] == 'subscribed');
+        @$tag_exist = (strpos($mc_result_add_member_to_tag['errors'][0]['error'], "already exist"));
+        @$tag_added = ($mc_result_add_member_to_tag['members_added'][0]['status'] == 'subscribed');
 
         if (!$tag_exist and !$tag_added) {
             return return_error('Mailchimp Subscribe', 'Error on add member to tag!', $mc_result_add_member_to_tag);
         }
-        // return return_success("DUMP", "DUMP", $mc_result_add_member_to_tag);
     }
 }
