afe77e7adcd92ae7ffa904b1f2292f5828c0a298
Fernando Ortiz Mello <guestbyte@gmail.com>
2020-03-09 12:42:36 -0300

    new functions, is_client, mth_is_onhold_list, include requirements

--- PRODUCAO/api/homolog/functions.php
+++ PRODUCAO/api/homolog/functions.php
@@ -10,6 +10,8 @@ require 'lib/mailchimp_get_segments.php';
 require 'lib/mailchimp_create_tags.php';
 require 'lib/mailchimp_add_member_to_tag.php';
 require 'lib/mailchimp_subscribe.php';
+require 'lib/mailchimp_member_exist.php';
+require 'lib/mth_campaign_plano.php';
 
 /**
  * Undocumented function
@@ -69,4 +71,44 @@ function return_error(string $name, string $description, $data = '')
     wh_log("$name: $description\n" . print_r($data, true));
     header('HTTP/1.1 400 Bad Request');
     return json_encode($return, JSON_UNESCAPED_UNICODE);
+}
+
+/**
+ * Check if order type is from a client
+ *
+ * @param [type] $order
+ * @return boolean
+ */
+function is_client($order) {
+    
+    $is_paying_customer = get_paying_customer($order->customer_id);
+    $order_zero_cart = ($order->total == '0.00');
+    $order_completed = ($order->status == 'completed');
+    $is_client = ($order_completed and ($is_paying_customer and !$order_zero_cart));
+
+    return $is_client;
+}
+
+/**
+ * Return if Order is typed to be 
+ *
+ * @param [type] $order
+ * @return boolean
+ */
+function mth_is_onhold_list($order, $order_payment_method) {
+
+    $order_boleto = ($order_payment_method == 'Boleto');
+    $order_onhold = ($order->status == 'on-hold');
+    $order_pending = ($order->status == 'pending');
+
+    $order_onhold_boleto = ($order_onhold and $order_boleto);
+    $order_pending_boleto = ($order_pending and $order_boleto);
+    
+    $is_onhold_list = ($order_onhold_boleto or $order_pending_boleto);
+    
+    if ($is_onhold_list) {
+         wh_log("Order status: $order->status | Payment method: $order_payment_method. Subscribing on different audience: $mc_list_id");
+     }
+     
+    return $is_onhold_list;
 }
