16a7c7dcc8d41a3467876b6853f8778fe7624eb5
Fernando Ortiz Mello <guestbyte@gmail.com>
2020-03-09 12:45:18 -0300

    refactoring mailchimp_member_exist, fix log return, is_client function, mth_is_onhold_list function

--- PRODUCAO/api/homolog/lib/route_woocommerce_webhooks.php
+++ PRODUCAO/api/homolog/lib/route_woocommerce_webhooks.php
@@ -12,14 +12,14 @@ function route_woocommerce_webhooks($order_id = false)
     $rawData = file_get_contents("php://input");
     $jsonData = json_decode($rawData);
 
-    $order_id = '69126';
+    $order_id = '69126'; // for debugging proposes 
 
     if ($order_id) {
-        $jsonData->id = $order_id;
-        wh_log("Manually process order: " . $jsonData->arg);
+        @$jsonData->id = $order_id;
+        wh_log("Manually process order: " . @$jsonData->arg);
     }
 
-    $order_id = ($jsonData->id) ? $jsonData->id : $jsonData->arg;
+    $order_id = ($jsonData->id) ? $jsonData->id : @$jsonData->arg;
 
     if (!isset($order_id)) {
             return return_error('route_woocommerce_webhooks', 'Error identifying order id!', $jsonData);
@@ -53,16 +53,11 @@ function route_woocommerce_webhooks($order_id = false)
     $mc_array['merge_fields']['CNPJ'] = $order->billing->cnpj;
     $mc_array['merge_fields']['TIPOCLIENT'] = $order->billing->persontype;
     $mc_array['merge_fields']['ORDER_ID'] = $order->id;
-
     // $has_coupon = (isset($order->coupon_lines[0]->code));
 
-    $is_paying_customer = get_paying_customer($order->customer_id);
-    $order_zero_cart = ($order->total == '0.00');
-    $order_completed = ($order->status == 'completed');
-    $is_client = ($order_completed and ($is_paying_customer and !$order_zero_cart));
+    $is_client = is_client($order);
     $mc_array['merge_fields']['CLIENTE'] = ($is_client) ? 'SIM' : 'NAO';
     
-    // return json_encode($mc_array, JSON_UNESCAPED_UNICODE);
     $member_tags = [];
     foreach ($order->line_items as $item) {
         unset($product);
@@ -71,10 +66,7 @@ function route_woocommerce_webhooks($order_id = false)
         $product = WP_API("GET", "/wc/v3/products/" . $item->product_id . "?");
 
         if (!isset($product->id)) {
-            wh_log("Error retrieving product data: \n" . print_r($product, true));
-            $return['error_product'] = 'Error on retrieving order data.';
-            // header('HTTP/1.1 500 Internal Server Error');
-            continue;
+              return return_error('route_woocommerce_webhooks', 'Error retrieving product data: ', $product);
         }
 
         array_push($member_tags, $item->sku);
@@ -123,140 +115,24 @@ function route_woocommerce_webhooks($order_id = false)
     }
 
     // CAMPANHA: Quem compra uma Plano, ganha de presente um curso gratis. 
-    // Esta funÃ§Ã£o identifica se o produto comprado Ã© um dos Planos, cria um cupom
-    // if (@$mth_campanha_plano) {
-    //     wh_log("CAMPANHA: Compre Plano ganhe Curso...");
-    //     $chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZ';
-    //     $code = 'MTH_' . substr(str_shuffle($chars), 0, 3);
-    //     $percent = 100;
-    //     $date_expires = '2020-01-31';
-    //     $product_categories = 43; // Curso 10h
-    //     $use_limit = 1; // quantas vezes pode ser usado
-    //     $description = 'API: Campanha Compre Plano ganhe Curso. Cliente: ' . $order->billing->email;
-
-    //     $coupom = mth_create_coupom($code, $percent, $date_expires, $product_categories, $use_limit, $description);
-
-    //     if (isset($coupom->code)) {
-    //         wh_log("Coupom created: " . $coupom->code);
-    //         $mc_array['merge_fields']['CUPOM_PRES'] = $coupom->code;
-    //     } else {
-    //         wh_log("Error create new coupom: \n" . print_r($coupom, true));
-    //         $return['error_coupom'] = 'Error create new coupom.';
-    //     }
-    // }
-
-    $order_onhold = ($order->status == 'on-hold');
-    $order_pending = ($order->status == 'pending');
-    $order_boleto = ($order_payment_method == 'Boleto');
-
-    $order_onhold_boleto = ($order_onhold and $order_boleto);
-    $order_pending_boleto = ($order_pending and $order_boleto);
-
-    $mc_list_id = ($order_onhold_boleto or $order_pending_boleto) ? $mc_onhold_list_id : $mc_customers_list_id;
-
-    if ($order_onhold_boleto) {
-        wh_log("Order status: $order->status | Payment method: $order_payment_method. Subscribing on diferent audience: $mc_list_id");
-    }
+    // TODO mth_campaign_plano: função ainda em teste
+    // mth_campaign_plano();
+    
+    $mc_list_id = (mth_is_onhold_list($order, @$order_payment_method)) ? $mc_onhold_list_id : $mc_customers_list_id;
 
     $mc_result = $MailChimp->post("lists/$mc_list_id/members", $mc_array);
 
     $member_subscribed = ($mc_result['status'] == 'subscribed');
     if ($member_subscribed) {
-        wh_log("Success subscribed to Mailchimp!");
-        header('HTTP/1.1 200 OK');
+        return return_success("route_woocommerce_webhooks", "Success subscribed to Mailchimp!", $mc_result);
     }
 
     $member_exists = ($mc_result['title'] == 'Member Exists');
     if ($member_exists) {
-        //TODO Refatorar para funções globais
-
-        wh_log($mc_result['title'] . ": Trying to update data...");
-
-        $subscriber_hash = md5($order->billing->email);
-        $put_result = $MailChimp->put("lists/$mc_list_id/members/$subscriber_hash", $mc_array);
-
-        if ($put_result['status'] !== 'subscribed') {
-            wh_log("Mailchimp Error: \n" . print_r($put_result, true));
-            wh_log("dump array posted on update: \n" . print_r($mc_array, true));
-            $return['error_mailchimp'] = 'Error on subscribing to Mailchimp.';
-            $return['return'] = $put_result;
-            // header('HTTP/1.1 500 Internal Server Error');
-            return json_encode($return, JSON_UNESCAPED_UNICODE);
-        }
-
-        wh_log("Member data updated, tryng to update TAGs...");
-
-        $mc_results = $MailChimp->get("lists/$mc_list_id/segments?count=1000");
-
-        $mc_results_no_statics = array_filter($mc_results['segments'], function ($v, $k) {
-            return $v['type'] == 'static';
-        }, ARRAY_FILTER_USE_BOTH);
-
-        $mc_list_tags_names = [];
-        foreach ($mc_results_no_statics as $key) {
-            array_push($mc_list_tags_names, $key['name']);
-        }
-
-        $tags_to_create_on_list = array_diff($mc_array['tags'], $mc_list_tags_names);
-
-        foreach ($tags_to_create_on_list as $tag) {
-            wh_log("Creating a TAG on list: " . $tag);
-
-            $mc_array['name'] = $tag;
-            $mc_array['static_segment'] = [];
-            $mc_result_add_tag_to_list = $MailChimp->post("lists/$mc_list_id/segments/", $mc_array);
-
-            if ($mc_result_add_tag_to_list['name'] !== $tag) {
-                wh_log("Mailchimp Error: \n" . print_r($put_result, true));
-                wh_log("dump array posted on update: \n" . print_r($mc_array, true));
-                $return['error_mailchimp'] = 'Error on subscribing to Mailchimp.';
-                $return['return'] = $put_result;
-                // header('HTTP/1.1 500 Internal Server Error');
-                return json_encode($return, JSON_UNESCAPED_UNICODE);
-            }
-            wh_log("TAG created: " . $tag);
-            sleep(2);
-        }
-
-        $mc_list_tags_names_ids = [];
-        foreach ($mc_results_no_statics as $key) {
-            $mc_list_tags_names_ids[$key['id']] = $key['name'];
-        }
-
-        foreach ($mc_array['tags'] as $member_tag) {
-            $tag_id = array_search($member_tag, $mc_list_tags_names_ids);
-            if (!isset($tag_id)) {
-                continue;
-            }
-
-            $data['members_to_add'] = [$order->billing->email];
-            $mc_result_add_member_to_tag = $MailChimp->post("lists/$mc_list_id/segments/" . $tag_id, $data);
-
-            if ($mc_result_add_member_to_tag['members_added'][0]['status'] == 'subscribed') {
-                wh_log("Success on update TAG: $member_tag");
-            } else {
-                wh_log("Error on update tag $member_tag : " . $mc_result_add_member_to_tag);
-            }
-
-            unset($tag_id);
-        }
-
-        $return['status'] = '200';
-        // wh_log("Success updated to Mailchimp!\n" . print_r($put_result, true));
-        wh_log("Success updated to Mailchimp!");
-        wh_log('------------------[ route_woocommerce_webhooks ]------------------ END');
-        return json_encode($put_result, JSON_UNESCAPED_UNICODE);
+        return mailchimp_member_exist($mc_result, $order->billing->email, $mc_list_id, $mc_array);
     }
 
     if (!$member_exists and !$member_subscribed) {
-        wh_log("Mailchimp Error: \n" . print_r($mc_result, true));
-        wh_log("dump array posted: \n" . print_r($mc_array, true));
-        $return['error_mailchimp'] = 'Error on subscribing to Mailchimp.';
-        // header('HTTP/1.1 500 Internal Server Error');
-        return json_encode($mc_result, JSON_UNESCAPED_UNICODE);
+        return return_error('route_woocommerce_webhooks', 'Error on subscribing to Mailchimp. ', $mc_result);
     }
-
-    $return['status'] = '200';
-    wh_log('------------------[ route_woocommerce_webhooks ]------------------ END');
-    return json_encode($return, JSON_UNESCAPED_UNICODE);
 }
