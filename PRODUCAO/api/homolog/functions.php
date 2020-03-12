13c626054ca4e3eb0ac05e0947ba90034e5d731c
Fernando Ortiz Mello <guestbyte@gmail.com>
2020-03-12 13:36:04 -0300

    fix API reference

--- PRODUCAO/api/homolog/functions.php
+++ PRODUCAO/api/homolog/functions.php
@@ -4,13 +4,7 @@ require 'lib/route_woocommerce_webhooks.php';
 require 'lib/route_woocommerce_webhooks_bulk.php';
 require 'lib/mth_create_coupom.php';
 require 'lib/MTH_API.php';
-require 'lib/WP_API.php';
 require 'lib/MTH_get_jwt_token.php';
-require 'lib/mailchimp_get_segments.php';
-require 'lib/mailchimp_create_tags.php';
-require 'lib/mailchimp_add_member_to_tag.php';
-require 'lib/mailchimp_subscribe.php';
-require 'lib/mailchimp_member_exist.php';
 require 'lib/mth_campaign_plano.php';
 
 /**
@@ -21,7 +15,9 @@ require 'lib/mth_campaign_plano.php';
  */
 function get_paying_customer(int $id)
 {
-    $result = WP_API("GET", "/wc/v3/customers/$id?");
+    global $API;
+
+    $result = $API->request("GET", "/wc/v3/customers/$id?");
     return $result->is_paying_customer;
 }
 
