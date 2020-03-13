a14f4ca0de42d3eb5c666e33c09fdf0fd511ee66
Fernando Ortiz Mello <guestbyte@gmail.com>
2020-03-13 13:59:39 -0300

    implemented unsubscribe and detelePermanent functions on Mailchimp class

--- PRODUCAO/api/homolog/lib/MailChimp.extends.class.php
+++ PRODUCAO/api/homolog/lib/MailChimp.extends.class.php
@@ -126,59 +126,109 @@ class MTH_Mailchimp extends MailChimp {
 
         $this->add_member_to_tag($mc_list_tags_names_ids, $mc_list_id, $mc_array);
 
-        return $API->return_success("Mailchimp Subscribe", "updated", $put_result);
+        return $API->return_success("Mailchimp Subscribe (member_exist)", "updated");
     }
 
     /**
- * Undocumented function
- *
- * @param string $mc_list_id
- * @param array $mc_array
- * @return void
- */
-function subscribe(string $mc_list_id, array $mc_array)
-{
-    global $MailChimp;
-    global $API;
+     * Undocumented function
+     *
+     * @param string $mc_list_id
+     * @param array $mc_array
+     * @return void
+     */
+    function subscribe(string $mc_list_id, array $mc_array)
+    {
+        global $MailChimp;
+        global $API;
 
-    $status_options = array('subscribed', 'unsubscribe');
-    $valid_status = (in_array($mc_array['status'], $status_options));
-    $valid_email = (filter_var($mc_array['email_address'], FILTER_VALIDATE_EMAIL));
+        $status_options = array('subscribed', 'unsubscribe');
+        $valid_status = (in_array($mc_array['status'], $status_options));
+        $valid_email = (filter_var($mc_array['email_address'], FILTER_VALIDATE_EMAIL));
 
-    $params_valid = ($valid_email and isset($mc_list_id) and $valid_status);
-    if (!$params_valid) {
-        return $API->return_error('Mailchimp Subscribe', 'Params passed is not valid!', $mc_array);
-    }
-
-    $result = $MailChimp->post("lists/$mc_list_id/members", $mc_array);
-
-    $success_subscribe = ($result['status'] == 'subscribed');
-    if ($success_subscribe) {
-        return $API->return_success("Mailchimp Subscribe", "subscribed", $result);
-    }
-
-    $member_exists = ($result['title'] == 'Member Exists');
-    if ($member_exists) {
-
-        $subscriber_hash = md5($mc_array['email_address']);
-        $result = $MailChimp->put("lists/$mc_list_id/members/$subscriber_hash", $mc_array);
-
-        $update_error = ($result['status'] !== 'subscribed');
-        if ($update_error) {
-            return $API->return_error('Mailchimp Subscribe', 'Error on update!', $result);
+        $params_valid = ($valid_email and isset($mc_list_id) and $valid_status);
+        if (!$params_valid) {
+            return $API->return_error('Mailchimp Subscribe', 'Params passed is not valid!', $mc_array);
         }
 
-        list($mc_list_tags_names_ids, $mc_list_tags_names) = $MailChimp->get_segments($mc_list_id);
+        $result = $MailChimp->post("lists/$mc_list_id/members", $mc_array);
 
-        $tags_to_create_on_list = array_diff($mc_array['tags'], $mc_list_tags_names);
+        $success_subscribe = ($result['status'] == 'subscribed');
+        if ($success_subscribe) {
+            return $API->return_success("Mailchimp Subscribe", "subscribed", $result);
+        }
 
-        $MailChimp->create_tags($tags_to_create_on_list, $mc_list_id);
+        $member_exists = ($result['title'] == 'Member Exists');
+        if ($member_exists) {
 
-        $MailChimp->add_member_to_tag($mc_list_tags_names_ids, $mc_list_id, $mc_array);
+            $subscriber_hash = md5($mc_array['email_address']);
+            $result = $MailChimp->put("lists/$mc_list_id/members/$subscriber_hash", $mc_array);
 
-        return $API->return_success("Mailchimp Subscribe", "updated", $result);
+            $update_error = ($result['status'] !== 'subscribed');
+            if ($update_error) {
+                return $API->return_error('Mailchimp Subscribe', 'Error on update!', $result);
+            }
+
+            list($mc_list_tags_names_ids, $mc_list_tags_names) = $MailChimp->get_segments($mc_list_id);
+
+            $tags_to_create_on_list = array_diff($mc_array['tags'], $mc_list_tags_names);
+
+            $MailChimp->create_tags($tags_to_create_on_list, $mc_list_id);
+
+            $MailChimp->add_member_to_tag($mc_list_tags_names_ids, $mc_list_id, $mc_array);
+
+            return $API->return_success("Mailchimp Subscribe (function)", "updated", $result);
+        }
+
+        return $API->return_error('Mailchimp Subscribe', 'Internal error: Mailchimp response not valid!', $result);
     }
 
-    return $API->return_error('Mailchimp Subscribe', 'Internal error: Mailchimp response not valid!', $result);
-}
+    /**
+     * Unsubscribe a member to a list
+     *
+     * @param string $mc_list_id
+     * @param [type] $mc_array
+     * @return void
+     */
+    function unsubscribe(string $mc_list_id, $mc_array)
+    {
+        global $MailChimp; 
+        global $API;
+        
+        $array['status'] = 'unsubscribed';
+        
+        $subscriber_hash = md5($mc_array['email_address']);
+        $result = $this->put("lists/$mc_list_id/members/$subscriber_hash", $array);
+            
+        $success = ($result['status'] == 'unsubscribed');
+        if ($success) {
+            return $API->return_success("Mailchimp Unsubscribe", "Unsubscribed from OnHold list of MailChimp");
+        }
+
+        return $API->return_error('Mailchimp Unsubscribe', 'Fail to unsubscribe to OnHold list.', $result);
+    }
+
+    /**
+     * Delete all personally identifiable information related to a list member, and remove them from a list. This will make it impossible to re-import the list member.
+     *
+     * @param string $mc_list_id
+     * @param [type] $mc_array
+     * @return void
+     */
+    function deletePermanent(string $mc_list_id, $mc_array)
+    {
+        global $MailChimp; 
+        global $API;
+        
+        $array['status'] = 'unsubscribed';
+        
+        $subscriber_hash = md5($mc_array['email_address']);
+        $result = $this->post("lists/$mc_list_id/members/$subscriber_hash/actions/delete-permanent", $array);
+            
+        $NotFound = ($result['status'] == '404');
+        if ($NotFound) {
+            return $API->return_error("Mailchimp Deleted", "Fail. The requested resource could not be found.");
+        }
+
+        return $API->return_success('Mailchimp Deleted', 'Success to delete resource.', $result);
+    }
 }
--- PRODUCAO/api/homolog/lib/route_woocommerce_webhooks.php
+++ PRODUCAO/api/homolog/lib/route_woocommerce_webhooks.php
@@ -14,7 +14,7 @@ function route_woocommerce_webhooks($order_id = false)
     $rawData = file_get_contents("php://input");
     $jsonData = json_decode($rawData);
 
-    $order_id = '77141'; // for debugging proposes 
+    // $order_id = '77141'; // for debugging proposes 
 
     if ($order_id) {
         @$jsonData->id = $order_id;
@@ -30,7 +30,7 @@ function route_woocommerce_webhooks($order_id = false)
     wh_log("Processing order: " . @$order_id);
 
     $order = $API->request("GET", "/wc/v3/orders/" . $order_id . "?");
-    
+
     if (!isset($order->id)) {
         return $API->return_error('route_woocommerce_webhooks', 'Error retrieving order data', $order);
     }
@@ -41,19 +41,31 @@ function route_woocommerce_webhooks($order_id = false)
     // TODO campaign_plano: função ainda em teste
     // $MTH->campaign_plano();
 
-    $mc_array = $MTH->prepare_data_to_mailchimp($order);
+    list($mc_array, $order_payment_method) = $MTH->prepare_data_to_mailchimp($order);
     
-    $mc_list_id = ($MTH->is_onhold_list($order, @$order_payment_method)) ? $mc_onhold_list_id : $mc_customers_list_id;
-
+    
+    $is_onhold_list = $MTH->is_onhold_list($order, @$order_payment_method);
+    
+    $mc_list_id = ($is_onhold_list) ? $mc_onhold_list_id : $mc_customers_list_id;
+    
+    wh_log("Order status: $order->status | Payment method: $order_payment_method. Reference list ID: $mc_list_id");
+    
     $mc_result = $MailChimp->post("lists/$mc_list_id/members", $mc_array);
-
+    
+    $order_boleto_completed = ($order_payment_method == 'Boleto' and $order->status == 'completed');
     $member_subscribed = ($mc_result['status'] == 'subscribed');
     if ($member_subscribed) {
+
+        ($order_boleto_completed) ? $MailChimp->deletePermanent($mc_onhold_list_id, $mc_array) : false ;
+
         return $API->return_success("route_woocommerce_webhooks", "Success subscribed to Mailchimp!", $mc_result);
     }
-
+    
     $member_exists = ($mc_result['title'] == 'Member Exists');
     if ($member_exists) {
+
+        ($order_boleto_completed) ? $MailChimp->deletePermanent($mc_onhold_list_id, $mc_array) : false ;
+
         return $MailChimp->member_exist($mc_result, $order->billing->email, $mc_list_id, $mc_array);
     }
 
